f4bcc1d9068fae85d9391bd3bc2ef6258789a36d
Matute0512 <matiast999@hotmail.com>
2026-05-21 23:16:59 -0300

    db: agregar tablas sales y sale_details para el historial de ventas

--- schema.sql
+++ schema.sql
@@ -1,12 +1,27 @@
 -- We create the database if it doesn´t exist and put it into use
 CREATE DATABASE IF NOT EXISTS gestion_inventario;
 USE gestion_inventario;
-
 -- We Created the 'products' table
 CREATE TABLE IF NOT EXISTS products(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL DEFAULT 0
+);
+-- 1. Main sales table (The Ticket)
+CREATE TABLE sales(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sale_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL
+);
+-- 2. Details table (The lines on the ticket)
+CREATE TABLE sale_details(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sale_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (sale_id) REFERENCES sales(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
